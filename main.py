0c133bbe3535e2eb43be7bf72de70025d9108d7e
tawainfer <tawainfer@gmail.com>
2024-04-29 15:45:43 +0900

    ソースコードを取得するメソッドを作成

--- main.py
+++ main.py
@@ -17,6 +17,28 @@ class Submission:
     self.__result = result
     self.__user_id = user_id
 
+    self.__url = f'https://atcoder.jp/contests/{self.__contest_id}/submissions/{self.__id}'
+    self.__code = None
+
+  def get_id(self):
+    return self.__id
+
+  def get_url(self):
+    return self.__url
+
+  def get_code(self):
+    if self.__code is None:
+      self.__scripe()
+    return self.__code
+
+  def __scripe(self):
+    time.sleep(1)
+    data = requests.get(self.__url)
+    html = BeautifulSoup(data.text, 'html.parser')
+    element = html.select_one('#submission-code')
+    lines = element.text.replace('\r\n', '\n').split('\n')
+    self.__code = '\n'.join(lines)
+
 class AtcoderUserData:
   def __init__(self, user):
     self.__user = user
@@ -43,7 +65,17 @@ class AtcoderUserData:
 class Main:
   def __init__(self):
     h = AtcoderUserData('tawainfer')
-    print(len(h.get_submissions()))
+    s = h.get_submissions()
+    print(f'{len(s)}件の提出履歴')
+    
+    for i in range(2):
+      for j in range(3):
+        st = time.time()
+        str = s[-(j + 1)].get_code()
+        et = time.time()
+        print(f'提出#{s[-(j + 1)].get_id()}のソースコードを取得 {len(str)}byte {et - st:.2f}s')
+        print(s[-(j + 1)].get_url())
+        print()
 
 if __name__ == '__main__':
   Main()
