7048efcf3caa1608dc7bdbc5bdf75bf208374a14
tawainfer <tawainfer@gmail.com>
2024-05-05 09:32:28 +0900

    リトライ処理ありのリクエストを送れるRequestSessionクラスを作成 #2

--- main.py
+++ main.py
@@ -9,6 +9,37 @@ import tempfile
 import time
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+class RequestSession():
+  __instance = None
+  __session = None
+
+  def __new__(cls):
+    if not cls.__instance:
+      cls.__instance = super(RequestSession, cls).__new__(cls)
+      cls.__session = cls.create_session()
+    return cls.__instance
+
+  @classmethod
+  def create_session(cls):
+    session = requests.Session()
+    session.mount('https://', HTTPAdapter())
+    return session
+
+  @classmethod
+  def get(cls, url, retry = 0, interval = 1, backoff = 1):
+    print(url, f'turn{5 - retry + 1}', f'wait = {interval}s')
+    time.sleep(interval)
+    try:
+      res = cls.__session.get(url)
+      res.raise_for_status()
+      return res
+    except requests.exceptions.RequestException as e:
+      if retry >= 1:
+        return cls.get(url, retry - 1, interval * backoff, backoff)
+      return None
 
 class LanguageUsedInAtcoder:
   language_to_extension = None
@@ -82,7 +113,7 @@ class Submission:
 
   def __scripe(self):
     time.sleep(1)
-    data = requests.get(self.__url)
+    data = RequestSession().get(self.__url, 5, 1, 1.5)
     html = BeautifulSoup(data.text, 'html.parser')
     element = html.select_one('#submission-code')
     lines = element.text.replace('\r\n', '\n').split('\n')
@@ -99,7 +130,7 @@ class AtcoderUserData:
 
   def __fetch_submissions(self, second):
     time.sleep(1)
-    data = requests.get(f'https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user={self.__user}&from_second={second}').json()
+    data = RequestSession().get(f'https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user={self.__user}&from_second={second}', 5, 1, 1.5).json()
 
     if len(data) == 0:
       return
@@ -179,3 +210,4 @@ class Main:
 
 if __name__ == '__main__':
   Main()
+
